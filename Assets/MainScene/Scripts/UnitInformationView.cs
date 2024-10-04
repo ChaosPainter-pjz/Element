a6a25b5d74d00226147a9af1997fc5acd4133119
Chaos <panjiazao@163.com>
2024-10-04 17:39:32 +0800

    继续完成主界面

---
+++ Assets/MainScene/Scripts/UnitInformationView.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitInformationView : MonoBehaviour
+{
+    void Init(HeroData heroData)
+    {
+
+    }
+}
